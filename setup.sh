62794db380ed33f6f37be3bb2e6f6dbeb8541b3d
Sebastian <duerr.sebastian@gmail.com>
2020-08-16 15:35:23 +0200

    changed sh file

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
-mkdir -p ~/.streamlit/ echo "\ [general]\n\ email = \"duerr.sebastian@gmail.com\"\n\ " > 
-~/.streamlit/credentials.toml echo "\ [server]\n\ headless = true\n\ enableCORS=false\n\
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"duerr.sebastian@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
